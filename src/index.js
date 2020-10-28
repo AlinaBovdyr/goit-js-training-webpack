148dca065ea47d04567314101b892a326710cede
Alina Bovdyr <alinabovdyr92@gmail.com>
2020-10-28 22:38:51 +0200

    Переносит страницу WebStudio на webpack

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import initMenu from './js/menu';
+import initModal from './js/modal';
+
+import './images/section-team/John.jpg';
+import './images/section-team/Marta.jpg';
+import './images/section-team/Martin.jpg';
+import './images/section-team/Robert.jpg';
+import './images/section-activity/computer.jpg';
+import './images/section-activity/laptop.jpg';
+import './images/section-activity/smartphone.jpg';
+import './images/Header.jpg';
+import './images/sprite.svg';
+
+import './sass/main.scss';
+
+initMenu();
+initModal();
